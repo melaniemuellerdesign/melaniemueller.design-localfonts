1ecab1bb9a9b6fdac5b81f7076fff46851d47d8d
Melanie Müller <contact@melaniemueller.design>
2023-10-15 17:56:51 +0200

    Add Pluginmenu

--- plugin.php
+++ plugin.php
@@ -9,19 +9,6 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-function add_plugin_menu()
-{
-	add_submenu_page(
-		'themes.php',          // Parent menu slug (Appearance)
-		'Localfonts',         // Page title
-		'Localfonts',         // Menu title
-		'manage_options',      // Capability required to access the menu
-		'localfonts-slug',    // Menu slug
-		'my_custom_localfonts_page'  // Callback function to display the menu content
-	);
-}
-add_action('admin_menu', 'add_plugin_menu');
-
 function custom_file_upload_form()
 {
 	ob_start();
@@ -155,3 +142,4 @@ $src_path = plugin_dir_path(__FILE__) . 'src/';
 
 // Include a specific file
 include($src_path . 'update.php');
+include($src_path . 'pluginmenu.php');
---
+++ src/pluginmenu.php
@@ -0,0 +1,14 @@
+<?php
+
+function add_plugin_menu()
+{
+    add_submenu_page(
+        'themes.php',          // Parent menu slug (Appearance)
+        'Localfonts',         // Page title
+        'Localfonts',         // Menu title
+        'manage_options',      // Capability required to access the menu
+        'localfonts-slug',    // Menu slug
+        'my_custom_localfonts_page'  // Callback function to display the menu content
+    );
+}
+add_action('admin_menu', 'add_plugin_menu');
